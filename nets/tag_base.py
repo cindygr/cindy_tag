ec3cc99cfef67dc2ca999f0dec99ba6bba86e991
cindygr <cindy.grimm@oregonstate.edu>
2025-05-15 16:08:15 -0700

    Trying to get to run

--- nets/tag_base.py
+++ nets/tag_base.py
@@ -599,8 +599,7 @@ class ConvDecoder3d(nn.Module):
     
 class Tag(nn.Module):
     def __init__(self, S, H, W, tstride=4, sstride=32, scales=[0.25,1.0]):
-        self.Module.__init__()
-        # super(nn.Module, self).__init__()
+        super(nn.Module, self).__init__()
         
         self.S = S
         self.H = H
